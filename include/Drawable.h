7242a699df5f6f11ec2139385457802cd1cb6f0c
ado_mayo1024 <git.hub.leon@gmail.com>
2025-05-30 20:02:00 +0200

    Drawable Kommentare leicht überarbeitet

--- include/Drawable.h
+++ include/Drawable.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <SFML/Graphics/Drawable.hpp>
 
-/*
+/**
  * Ein Drawable ist ein Objekt welches auf ein SFML-Window gezeichnet werden kann.
  */
 
@@ -11,25 +11,25 @@ public:
     Drawable() = default;
     virtual ~Drawable() = default;
 
-    /*
+    /**
      * Die Funktion die aufgerufen wird, wenn das Objekt gedrwat werden soll.
      * return: Gibt das sf::Drawable zurück welches dann auf das Winow gemalt werden soll
      */
     virtual sf::Drawable& draw() = 0;
 
-    /*
+    /**
      * Aktiviert oder deaktiviert, ob es gerade gemalt werden soll.
      */
     void switchActivity() {active %= 1;}
 
-    /*
-     *Gibt die Aktivität wieder ob es gemalt werden soll
+    /**
+     *Gibt die Aktivität wieder, ob es gemalt werden soll
      */
     bool isActive() const {return active;}
 
     private:
-    /*
-     *Variable die beschreibt ob das Objekt gemalt werden soll
+    /**
+     *Variable die beschreibt, ob das Objekt gemalt werden soll
      */
     bool active = true;
 };
