c22d44f21bac3d39a2c7cc9c7bd961c8e9b1c214
ado_mayo1024 <git.hub.leon@gmail.com>
2025-05-30 20:02:15 +0200

    DrawingManager kommentiert

--- include/DrawingManager.h
+++ include/DrawingManager.h
@@ -3,6 +3,10 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include "Drawable.h"
 
+/**
+ * Enum welche die verschiedenen Tiefen repräsentiert, wann etwas gezeichnet werden soll.
+ * (Wird wahrscheinlich aber noch ersetzt so dass ich eine undefinierte Anzahl an Layern habe)
+ */
 enum class LAYER {
     FARBACKGROUND,
     BACKGROUND,
@@ -10,16 +14,38 @@ enum class LAYER {
     FOREGROUND,
     NEARFOREGROUND
 };
-
+/**
+ * Die Klasse die alle Drawable Objekt managed und diese dann auch auf das Window malt
+ */
 class DrawingManager {
 
 public:
-    explicit DrawingManager(sf::RenderWindow&);
+    /**
+     * Konstruktor der ein DrawingManager Objekt initialisiert
+     * @param: window das Window auf das gemalt werden soll
+     */
+    explicit DrawingManager(sf::RenderWindow& window);
+
+    /**
+     * Malt alle Objekt die es managed und und aktiv sind auf das Window
+     */
     void draw();
+
+    /**
+     * Hier werden Drawable Objekte angemeldet  die auf das Window gemalt werden sollen
+     */
     void setDrawings(std::shared_ptr<Drawable>);
     std::vector<std::shared_ptr<Drawable>> getDrawings();
 private:
+    /**
+     * Dieser Vector hält alle Drawable Objekte die vom Drawing Manager gemanaged werden.
+     * Der Vektor hält diese als shared_pointer um Polymorphism zu bewerkstelligen.
+     */
     std::vector<std::shared_ptr<Drawable>> drawings;
+
+    /**
+     * Das Window wo drauf der DrawingManager alles drauf malt
+     */
     sf::RenderWindow& window;
 
 
