b51bad97b1a7a27f7581823b242e8fac42b39bd5
ado_mayo1024 <git.hub.leon@gmail.com>
2025-06-02 19:33:17 +0200

    Updatable.h kommentiert

--- include/Updatable.h
+++ include/Updatable.h
@@ -1,8 +1,16 @@
 #pragma once
 
+/**
+ * Klasse Updatable ist eine abstrakte Klasse, welche Objekte definiert, die, in der regel, nach jedem Frame
+ * geupdated werden sollen
+ */
 class Updatable {
 public:
     Updatable() = default;
     virtual ~Updatable() = default;
+
+    /**
+     * Die Updatefunktion die das Objekt updated
+     */
     virtual void update() = 0;
 };
