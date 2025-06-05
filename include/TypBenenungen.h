9b0b661ccb11a64a440ee3ed1f19faf72ef18031
adomayo1024 <git.hub.leon@gmail.com>
2025-06-05 11:13:20 +0200

    TypBennungen.h kommentiert

--- include/TypBenenungen.h
+++ include/TypBenenungen.h
@@ -2,6 +2,9 @@
 #include <functional>
 #include <SFML/Window/Event.hpp>
 
+/**
+ * Datei die lange Typen in einfache name definiert
+ */
 using inputHandlerFunktion = std::function<void(const sf::Event &, float)>;
 using Key = sf::Keyboard::Key;
 using EventType = sf::Event::EventType;
