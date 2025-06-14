55a6b03d052bb476ae4e974769b6a40ee2db0f29
ado_mayo1024 <git.hub.leon@gmail.com>
2025-06-14 22:51:10 +0200

    weitere test für Inputmanager geschrieben.

--- tests/InputMangerTest.cpp
+++ tests/InputMangerTest.cpp
@@ -3,6 +3,8 @@
 #include "InputManager.h"
 #include <SFML/Window/Event.hpp>
 
+#include "Storage.h"
+
 namespace myGE {
     TEST(InputManagerTest, InitializeWithNothing) {
         InputManager inputManager{};
@@ -20,15 +22,52 @@ namespace myGE {
         EXPECT_EQ(1, inputManager.keyMap.size());
     }
 
-    TEST(InputManagerTest, Manage) {
+    /**
+     * Testet, ob bei einem nicht Key event auch alle Objekte benachrichtigt werden, die sich dafür angemeldet haben
+     * und deren Funktionen aufgerufen werden, mit denen sie sich angemeldet haben.
+     */
+    TEST(InputManagerTest, ManageNonKeyInput) {
+        InputableMock mock{};
+        std::vector<std::tuple<EventType, Key, inputHandlerFunktion>> inputHandlers = mock.giveEventListner();
+        InputManager inputManager{inputHandlers};
+        EXPECT_CALL(mock, mockNotKey(testing::_, testing::_))
+        .Times(1);
+        sf::Event event = sf::Event{EventType::MouseButtonPressed};
+        inputManager.manage(event, 12);
+
+    }
+
+    /**
+     * Testet, ob bei einem Key Event die Objekte noch nicht über das Event benachrichtigt werden.
+     * Aber der Key als gedrückt registriert wird, im Storage.
+     */
+    TEST(InputManager, ManageKeyInput) {
         InputableMock mock{};
         std::vector<std::tuple<EventType, Key, inputHandlerFunktion>> inputHandlers = mock.giveEventListner();
         InputManager inputManager{inputHandlers};
         EXPECT_CALL(mock, mockKey(testing::_, testing::_))
         .Times(0);
-        EXPECT_CALL(mock, mockNotKey(testing::_, testing::_))
-        .Times(1);
-        sf::Event event = sf::Event{EventType::MouseButtonPressed};
+        sf::Event event = sf::Event{sf::Event::KeyPressed};
+        event.key.code = Key::Right;
         inputManager.manage(event, 12);
+        EXPECT_TRUE(Storage::containsKey(Key::Right));
+    }
+
+    /**
+     * Testet, ob auch alle Objekte, die sich für ein Key Event angemeldet haben, auch benachrichtigt werden und die
+     * Funktionen aufgerufen werden, mit der sie sich angemeldet haben.
+     */
+    TEST(InputManager, handleStillPressedKeys) {
+        InputableMock mock{};
+        std::vector<std::tuple<EventType, Key, inputHandlerFunktion>> inputHandlers =
+            mock.giveEventListner();
+        InputManager inputManager{inputHandlers};
+        EXPECT_CALL(mock, mockKey(testing::_, testing::_))
+        .Times(1);
+        sf::Event event = sf::Event{sf::Event::KeyPressed};
+        event.key.code = Key::Right;
+        inputManager.manage(event, 12);
+        EXPECT_TRUE(Storage::containsKey(Key::Right));
+        inputManager.handleStillPressedKeys(12);
     }
 }
