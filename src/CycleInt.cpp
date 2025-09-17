8e9c2f81f4f0b9cf8293d9cfbba2d2c2f447c967
ado_mayo1024 <git.hub.leon@gmail.com>
2025-09-17 21:18:28 +0200

    Cycle Int - und + operator implementiert

--- src/CycleInt.cpp
+++ src/CycleInt.cpp
@@ -18,6 +18,17 @@ myGE::CycleInt myGE::CycleInt::operator--(int) {
 
 int myGE::CycleInt::operator-(int o) {
     return (i - o) % max;
+    // wenn o > max geht, man eh einmal herum, dann kann man auch einfach nur die nötigen Schritte, die dann noch bleiben, gehen.
+    o = o % max;
+    // wenn o > i ist, würde es in den negativen Bereich gehen, deshalb zieht man den Wert, der bei i - o herauskommt, vom Max ab.
+    int res = (o <= i) ? (i - o) % max : max + (i - o);
+    return res;
+}
+
+int myGE::CycleInt::operator+(int o) {
+    o = o % max;
+    int res = (i + o >= max) ? 0 - (max - (i + o)) : i + o;
+    return res;
 }
 
 myGE::CycleInt::operator int() const {
